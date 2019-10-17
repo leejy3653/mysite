a2a4861bc877d9bace9250caa77f5efaea9d6a10
ljy <leejy3653@naver.com>
2019-10-17 10:42:57 +0900

    messageConfig

--- mysite4/src/main/java/kr/co/itcen/config/app/DBConfig.java
+++ mysite4/src/main/java/kr/co/itcen/config/app/DBConfig.java
@@ -14,7 +14,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
 @EnableTransactionManagement
-@PropertySource("classpath:kr/co/itcen/config/app/properties/jdbc.properties")
+@PropertySource("classpath:kr/co/itcen/mysite/config/properties/jdbc.properties")
 public class DBConfig {
 	
 	@Autowired
--- mysite4/src/main/java/kr/co/itcen/mysite/config/WebConfig.java
+++ mysite4/src/main/java/kr/co/itcen/mysite/config/WebConfig.java
@@ -6,13 +6,14 @@ import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.context.annotation.Import;
 
 import kr.co.itcen.config.web.MVCConfig;
+import kr.co.itcen.config.web.MessageConfig;
 import kr.co.itcen.config.web.SecurityConfig;
-
+	
 @Configuration
 //@EnableWebMvc //MVCConfig에서 해준다
 @EnableAspectJAutoProxy//<aop:aspectj-autoproxy />
 @ComponentScan({ "kr.co.itcen.mysite.controller" }) // auto-scan
-@Import({MVCConfig.class, SecurityConfig.class})
+@Import({MVCConfig.class, SecurityConfig.class, MessageConfig.class})
 public class WebConfig {
 	
 }
